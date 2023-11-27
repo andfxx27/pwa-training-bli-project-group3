79a26c4395c0a076ada9ed5d64ec3b0ed165eb77
andfxx27 <idea.andersen@gmail.com>
2023-11-27 21:39:10 +0700

    test 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ const count = ref(0);
 <template>
   <h1>PWA Training BLI Project Group 3</h1>
 
-  <p>Code changes #1</p>
+  <p>Code changes #2</p>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
